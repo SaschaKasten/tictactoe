0ca0cb520ad6b8e4e9d39b8f1c3f274310b612f8
Sascha Kasten <you@example.com>
2022-10-31 23:40:31 +0100

    up

--- script.js
+++ script.js
@@ -21,6 +21,22 @@ function fillShape(id) {
     }
 }
 
+function restart() {
+    gameOver = false;
+    fields = [];
+    document.getElementById('game-over').classList.add('d-none');
+    document.getElementById('restart-button').classList.add('d-none');
+
+    for (let i = 0; i < 8; i++) {
+        document.getElementById('line' + i).classList.add('d-none');
+    }
+
+    for (let i = 0; i < 9; i++) {
+        document.getElementById('circle-' + i).classList.add('d-none');
+        document.getElementById('cross-' + i).classList.add('d-none');
+    }
+}
+
 function draw() {
     for (let i = 0; i < fields.length; i++) {
         if (fields[i] == 'circle') {
@@ -39,7 +55,6 @@ function checkForWin() {
     if (fields[0] == fields[1] && fields[1] == fields[2] && fields[0]) {
         winner = fields[0];
         document.getElementById('line-1').style.transform = 'scalex(1)';
-
     }
     if (fields[3] == fields[4] && fields[4] == fields[5] && fields[3]) {
         winner = fields[3];
@@ -49,42 +64,37 @@ function checkForWin() {
     if (fields[6] == fields[7] && fields[7] == fields[8] && fields[6]) {
         winner = fields[6];
         document.getElementById('line-3').style.transform = 'scalex(1)';
-
     }
 
     if (fields[0] == fields[3] && fields[3] == fields[6] && fields[0]) {
         winner = fields[0];
         document.getElementById('line-4').style.transform = 'rotate(90deg) scalex(1)';
-
     }
 
     if (fields[1] == fields[4] && fields[4] == fields[7] && fields[1]) {
         winner = fields[1];
         document.getElementById('line-5').style.transform = 'rotate(90deg) scalex(1)';
-
     }
 
     if (fields[2] == fields[5] && fields[5] == fields[8] && fields[2]) {
         winner = fields[2];
         document.getElementById('line-6').style.transform = 'rotate(90deg) scalex(1)';
-
     }
     if (fields[0] == fields[4] && fields[4] == fields[8] && fields[0]) {
         winner = fields[0];
         document.getElementById('line-7').style.transform = 'rotate(45deg) scalex(1.2)';
-
     }
 
     if (fields[2] == fields[4] && fields[4] == fields[6] && fields[2]) {
         winner = fields[2];
         document.getElementById('line-8').style.transform = 'rotate(-45deg) scalex(1.2)';
-
     }
     if (winner) {
         console.log('GEWONNEN:', winner);
         gameOver = true;
         setTimeout(function () {
             document.getElementById('game-over').classList.remove('d-none');
+            document.getElementById('restart-button').classList.remove('d-none');
         }, 1000);
     }
 }
